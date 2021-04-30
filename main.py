efe026e44f3c0762e011f4c4e7c3f23bfa3c2aae
Robson <61189021+TheRobh12@users.noreply.github.com>
2021-04-29 22:31:09 -0300

    Atualization of codes

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
+import random
 
-#
+
 azul = (50, 100, 213)
 laranja = (205,102,0)
+verde = (0,255,0)
+
 dimensoes  = (600, 600)
 
-##VALORES INICIAIS##
+## VALORES INICIAIS ##
 
 x = 300
 y = 300
@@ -14,8 +17,11 @@ d = 20
 
 lista_cobra = [[x,y]]
 
+dx = 0
+dy = 0
 
-
+x_comida = round(random.randrange(0, 600 - d) / 20) * 20
+y_comida = round(random.randrange(0 , 600 - d) / 20) * 20
 
 tela = pygame.display.set_mode((dimensoes))
 pygame.display.set_caption('Snake da Kenzie')
@@ -24,37 +30,55 @@ tela.fill(azul)
 
 clock = pygame.time.Clock()
 
-def desenha_cobra():
+def desenha_cobra(lista_cobra):
     tela.fill(azul)
     for unidade in lista_cobra:
-    pygame.draw.rect(tela,laranja,[unidade 0],[unidade 1],d,d)
+        pygame.draw.rect(tela, laranja, [unidade[0], unidade[1], d, d])
 
-def mover_cobra():
-    delta_x = 0
-    delta_y = 0
+def mover_cobra(dx, dy, lista_cobra):
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                delta_x = -d
-                delta_y = 0
+                dx = -d
+                dy = 0
             elif event.key == pygame.K_RIGHT:
-                delta_x = d
-                delta_y = 0
+                dx = d
+                dy = 0
             elif event.key == pygame.K_UP:
-                delta_x = 0
-                delta_y = -d
+                dx = 0
+                dy = -d
             elif event.key == pygame.K_DOWN:
-                delta_x = 0
-                delta_y = d
+                dx = 0
+                dy = d
 
-x_novo = lista_cobra[- 1][0] + delta_x
-y_novo = lista_cobra[-1][0] + delta_y
+    x_novo = lista_cobra[- 1][0] + dx
+    y_novo = lista_cobra[-1][0] + dy
 
-lista_cobra.append([x_novo, y_novo])
+    lista_cobra.append([x_novo, y_novo])
 
+    del lista_cobra[0]
+
+    return dx, dy, lista_cobra
+
+def verifica_comida(dx, dy, x_comida, y_comida, lista_cobra):
+
+    head = lista_cobra[-1]
+
+    x_novo = head[0] + dx
+    y_novo = head[1] + dy
+
+    if head[0] == x_comida and head[1] == y_comida:
+        lista_cobra.append([x_novo, y_novo])
+
+    pygame.draw.rect(tela, verde, [x_comida, y_comida, d, d])
+
+    return x_comida, y_comida, lista_cobra
 
 while True:
     pygame.display.update()
-    desenha_cobra()
-    print('oi')
-    clock.tick(1)
+    desenha_cobra(lista_cobra)
+    dx, dy, lista_cobra = mover_cobra(dx,dy, lista_cobra)
+    x_comida, y_comida, lista_cobra = verifica_comida (dx, dy, x_comida, y_comida, lista_cobra)
+    print(lista_cobra)
+
+    clock.tick(20)
